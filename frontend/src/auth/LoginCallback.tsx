180df96745bf63e7cf7bf0cec1a2d9fe807ee03c
Mikael Solstad <mikaelrinosolstad@gmail.com>
2019-04-27 14:54:02 +0200

    Added logout functionality

--- frontend/src/auth/LoginCallback.tsx
+++ frontend/src/auth/LoginCallback.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import AuthContext from './AuthContext';
 import Spinner from '../components/shared/Spinner';
 
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  login: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
+
 const LoginCallback = () => {
   const auth = React.useContext(AuthContext);
   auth.handleAuthentication();
-  return <Spinner />;
+  return (
+    <div className={css(styles.login)}>
+      <div>Logging in...</div>
+      <Spinner />
+    </div>
+  );
 };
 
 export default LoginCallback;
