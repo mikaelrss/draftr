c6a9cb3b72c9d5f96ab890ad898caf687bda25d8
Mikael Solstad <mikaelrinosolstad@gmail.com>
2019-05-19 17:46:08 +0200

    Added view for view only owned ranks

--- frontend/src/components/appheader/AppHeader.tsx
+++ frontend/src/components/appheader/AppHeader.tsx
@@ -1,19 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Popup } from 'semantic-ui-react';
 import { css, StyleSheet } from 'aphrodite/no-important';
+
 import { PRIMARY, PRIMARY_TEXT } from '../../styles/colors';
 import { DEFAULT_PADDING, MOBILE_BREAKPOINT } from '../../styles/constants';
-import { Button } from '../shared/Button';
+import { Button, ClickableSurface } from '../shared/Button';
 import AuthContext from '../../auth/AuthContext';
 import ProfileInfo from '../profileinfo/ProfileInfo';
 import Icon, { IconType } from '../shared/Icon';
 import Link from '../shared/Link';
+import ProfileMenu from '../profilemenu/ProfileMenu';
 
 const ICON_SIZE = 40;
+export const HEADER_HEIGHT = 64;
 
 const styles = StyleSheet.create({
   header: {
     width: `calc(100% - ${DEFAULT_PADDING * 2})`,
-    height: '64px',
+    height: `${HEADER_HEIGHT}px`,
     backgroundColor: PRIMARY,
     display: 'flex',
     alignItems: 'center',
@@ -35,10 +39,16 @@ const styles = StyleSheet.create({
       alignItems: 'center',
     },
   },
+  click: {
+    display: 'flex',
+    width: 'auto !important',
+  },
 });
 
 const AppHeader = () => {
   const auth = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
+
   return (
     <header className={css(styles.header)}>
       <Link to="/ranks" className={css(styles.titleContainer)}>
@@ -46,7 +56,21 @@ const AppHeader = () => {
         <div>Draftr</div>
       </Link>
       {!auth.isAuthenticated() && <Button onClick={auth.login} value="Login" />}
-      {auth.isAuthenticated() && <ProfileInfo />}
+      {auth.isAuthenticated() && (
+        <Popup
+          open={open}
+          on="click"
+          content={<ProfileMenu />}
+          trigger={
+            <ClickableSurface className={css(styles.click)}>
+              <ProfileInfo />
+            </ClickableSurface>
+          }
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          position="bottom center"
+        />
+      )}
     </header>
   );
 };
---
+++ frontend/src/components/profilemenu/ProfileMenu.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Paper from '../shared/Paper';
+import { css, StyleSheet } from 'aphrodite/no-important';
+import Link from '../shared/Link';
+import Typography, { FontSize } from '../shared/Typography';
+import { SECONDARY_TEXT } from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '200px',
+  },
+  font: {
+    color: SECONDARY_TEXT,
+  },
+});
+const ProfileMenu = () => (
+  <Paper className={css(styles.container)}>
+    <Link to="/ranks">
+      <Typography className={css(styles.font)} size={FontSize.medium}>
+        All ranks
+      </Typography>
+    </Link>
+    <Link to="/my-ranks">
+      <Typography className={css(styles.font)} size={FontSize.medium}>
+        My ranks
+      </Typography>
+    </Link>
+  </Paper>
+);
+
+export default ProfileMenu;
